a1b8646994672f5adee2875cb9d685e72bfe2c51
HasturKiY <95368235+HasturKiY@users.noreply.github.com>
2025-09-18 11:32:04 +0300

    Продолжение работы
    Добавил вторую форму и придумал её

--- UD/UD/AddForm.vb
+++ UD/UD/AddForm.vb
@@ -1,3 +1,13 @@
 ﻿Public Class AddForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
--- UD/UD/Form1.vb
+++ UD/UD/Form1.vb
@@ -4,6 +4,12 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AddForm.Show()
+        Me.Hide()
 
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
     End Sub
 End Class
